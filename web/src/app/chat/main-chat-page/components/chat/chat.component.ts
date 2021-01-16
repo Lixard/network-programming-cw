1a5f247ddbb160845c02559c515e800e4ee398c1
Lixard <Lixard870@gmail.com>
2021-01-17 01:23:25 +0300

    Chat component css configuration

--- web/src/app/chat/main-chat-page/components/chat/chat.component.ts
+++ web/src/app/chat/main-chat-page/components/chat/chat.component.ts
@@ -3,6 +3,7 @@ import { Chat } from '../../../../models/chat.model';
 import { Observable } from 'rxjs';
 import { Message } from '../../../../models/message.model';
 import { ChatService } from '../../../../services/chat.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -11,12 +12,20 @@ import { ChatService } from '../../../../services/chat.service';
 })
 export class ChatComponent implements OnInit {
   @Input() chat: Chat;
+  form: FormGroup;
 
   messages$: Observable<Message[]>;
 
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.buildForm();
     this.messages$ = this.chatService.getChatMessages(this.chat.id);
+  }
+
+  private buildForm() {
+    this.form = this.fb.group({
+      message: this.fb.control('', Validators.required),
+    });
   }
 }
